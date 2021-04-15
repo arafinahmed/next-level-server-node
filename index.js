988c6bbf0f49b563a7937774f47837a14364f332
arafinahmed <arafin2021@gmail.com>
2021-04-16 03:36:12 +0600

    active student added to db

---
+++ index.js
@@ -0,0 +1,111 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+require('dotenv').config();
+const fs = require('fs-extra');
+
+const app = express()
+const port = 8888
+
+app.use(bodyParser.json())
+app.use(cors())
+app.use(express.static('doctors'));
+app.use(fileUpload())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2jkon.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+client.connect(err => {
+    const adminsCollection = client.db(process.env.DB_NAME).collection(process.env.DB_COLLECTION);
+    const coursesCollection = client.db(process.env.DB_NAME).collection(process.env.DB_COLLECTION2);
+    const studentsCollection = client.db(process.env.DB_NAME).collection(process.env.DB_COLLECTION3);
+    app.post('/addAdmin', (req, res) => {
+        try {
+            const admin = req.body.email;
+            adminsCollection.insertOne({ email: admin })
+                .then(result => {
+                    res.send(result.insertedCount > 0);
+                })
+        }
+        catch {
+            res.send(false);
+        }
+    })
+
+    app.post('/addCourse', (req, res) => {
+        try {
+            const file = req.files.file;
+            const title = req.body.title;
+            const fee = req.body.fee;
+            const shortDescription = req.body.shortDescription;
+            const newImg = file.data;
+            const encImg = newImg.toString('base64');
+            var img = {
+                contentType: file.mimetype,
+                size: file.size,
+                img: Buffer.from(encImg, 'base64')
+            };
+
+            coursesCollection.insertOne({ title, fee, shortDescription, img })
+                .then(result => {
+                    res.send(result.insertedCount > 0);
+                })
+        }
+        catch {
+            res.send(false);
+        }
+    });
+
+    app.get('/allCourses', (req, res) => {
+        try {
+            coursesCollection.find({})
+                .toArray((err, docs) => {
+                    res.send(docs);
+                })
+        }
+        catch {
+            res.send([]);
+        }
+    })
+
+    app.get('/course/:id', (req, res) => {
+        try {
+            const id = ObjectID(req.params.id);
+            console.log(id);
+            coursesCollection.find({ _id: id })
+                .toArray((err, document) => {
+                    res.send(document[0]);
+                })
+        }
+        catch {
+            res.send({})
+        }
+    });
+
+    app.post('/addStudent', (req, res) => {
+        try {
+            const data = req.body;
+            studentsCollection.insertOne(data)
+                .then(result => {
+                    res.send(result.insertedCount > 0);
+                })
+        }
+        catch{
+            res.send(false);
+        }
+        
+
+    });
+
+})
+
+app.listen(process.env.PORT || port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
